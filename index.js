caa5e42b75ebacafb329a2dc0d9eda538eccb315
khai96_ <hvksmr1996@gmail.com>
2017-04-07 20:51:19 +0700

    No wrapper

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
+'use strict'
 
-((module) => {
-  'use strict'
+const createClassFromSuper = require('simple-class-utils').createClass.super
+const recursiveConstructor = require('x-iterable-utils/recursive-constructor')
+const manySameElements = require('x-iterable-utils/many-same-elements')
+const ProductIterableSuper = require('x-iterable-utils/appx-super-class')(build, iterate)
 
-  const createClassFromSuper = require('simple-class-utils').createClass.super
-  const recursiveConstructor = require('x-iterable-utils/recursive-constructor')
-  const manySameElements = require('x-iterable-utils/many-same-elements')
-  const ProductIterableSuper = require('x-iterable-utils/appx-super-class')(build, iterate)
-
-  class ProductIterable extends ProductIterableSuper {
-    static times (...args) {
-      return ProductIterable.pow(...args)
-    }
-
-    static pow (iterable, exponent) {
-      const args = manySameElements(iterable, exponent)
-      return args.length ? new ProductIterable(...args) : []
-    }
+class ProductIterable extends ProductIterableSuper {
+  static times (...args) {
+    return ProductIterable.pow(...args)
   }
 
-  module.exports = ProductIterable
-
-  ProductIterable.Result = createClassFromSuper(Array)
-
-  function build (self, ...args) {
-    return recursiveConstructor(self, ProductIterable, (value) => [value], ...args)
+  static pow (iterable, exponent) {
+    const args = manySameElements(iterable, exponent)
+    return args.length ? new ProductIterable(...args) : []
   }
+}
 
-  function * iterate () {
-    const {first, second} = this
-    for (const i of first) {
-      for (const j of second) {
-        yield new ProductIterable.Result(i, ...j)
-      }
+module.exports = ProductIterable
+
+ProductIterable.Result = createClassFromSuper(Array)
+
+function build (self, ...args) {
+  return recursiveConstructor(self, ProductIterable, (value) => [value], ...args)
+}
+
+function * iterate () {
+  const {first, second} = this
+  for (const i of first) {
+    for (const j of second) {
+      yield new ProductIterable.Result(i, ...j)
     }
   }
-})(module)
+}
