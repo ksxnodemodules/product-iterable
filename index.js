721a6c6c58976bac47a332439649df173b7abc3d
khai96_ <hvksmr1996@gmail.com>
2017-04-07 20:15:13 +0700

    Run standard --fix

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 
 ((module) => {
-	'use strict';
+  'use strict'
 
-	var createClassFromSuper = require('simple-class-utils').createClass.super;
-	var recursiveConstructor = require('x-iterable-utils/recursive-constructor');
-	var manySameElements = require('x-iterable-utils/many-same-elements');
-	var ProductIterableSuper = require('x-iterable-utils/appx-super-class')(build, iterate);
+  var createClassFromSuper = require('simple-class-utils').createClass.super
+  var recursiveConstructor = require('x-iterable-utils/recursive-constructor')
+  var manySameElements = require('x-iterable-utils/many-same-elements')
+  var ProductIterableSuper = require('x-iterable-utils/appx-super-class')(build, iterate)
 
-	class ProductIterable extends ProductIterableSuper {
+  class ProductIterable extends ProductIterableSuper {
+    static times (...args) {
+      return ProductIterable.pow(...args)
+    }
 
-		static times(...args) {
-			return ProductIterable.pow(...args);
-		}
-
-		static pow(iterable, exponent) {
-			var args = manySameElements(iterable, exponent);
-			return args.length ? new ProductIterable(...args) : [];
-		}
-
+    static pow (iterable, exponent) {
+      var args = manySameElements(iterable, exponent)
+      return args.length ? new ProductIterable(...args) : []
+    }
 	}
 
-	module.exports = ProductIterable;
+  module.exports = ProductIterable
 
-	ProductIterable.Result = createClassFromSuper(Array);
+  ProductIterable.Result = createClassFromSuper(Array)
 
-	function build(self, ...args) {
-		return recursiveConstructor(self, ProductIterable, (value) => [value], ...args);
-	}
+  function build (self, ...args) {
+    return recursiveConstructor(self, ProductIterable, (value) => [value], ...args)
+  }
 
-	function * iterate() {
-        var {first, second} = this;
-		for (let i of first) {
-			for (let j of second) {
-				yield new ProductIterable.Result(i, ...j);
-			}
-		}
-	}
-
-})(module);
+  function * iterate () {
+    var {first, second} = this
+    for (let i of first) {
+      for (let j of second) {
+        yield new ProductIterable.Result(i, ...j)
+      }
+    }
+  }
+})(module)
